6a92e29f361281f21696c1bc86ea6e7ecee458be
VJ Jain <Vaibhav.Jain@Plenti.com.au>
2024-12-20 21:37:44 +0530

    Add Shlokas Content

---
+++ js/disableCopy.js
@@ -0,0 +1,17 @@
+﻿const contextMenuMethod = (event) => {
+    event.preventDefault();
+};
+
+window.disableCopy = window.disableCopy || function() {
+    document.addEventListener('contextmenu', contextMenuMethod);
+    document.onselectstart = function () {
+        return false;
+    };
+}
+
+window.enableCopy = window.enableCopy || function() {
+    document.removeEventListener('contextmenu', contextMenuMethod);
+    document.onselectstart = function () {
+        return true;
+    };
+}
